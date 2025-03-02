bac366bbc5471a20a6b63b1dd17c4c6b838df49c
mexic <mexic2284@mail.ru>
2025-03-03 00:24:13 +0400

    Добавлена кастомная модель пользователя и менеджер
    - Создана кастомная модель пользователя "User", где email используется как уникальный идентификатор.
    - Удалено поле "username", так как аутентификация происходит по email.
    - Добавлены поля "token" и "is_token_used" для управления подтверждением email.
    - Реализован кастомный менеджер "CustomUserManager":
      - Метод "create_user" создает пользователя и автоматически добавляет его в группу "User".
      - Метод "create_superuser" создает суперпользователя с флагами "is_staff" и "is_superuser".

--- users/models.py
+++ users/models.py
@@ -1,3 +1,54 @@
+from django.contrib.auth.models import AbstractUser, UserManager, Group
 from django.db import models
 
-# Create your models here.
+
+class CustomUserManager(UserManager):
+	""" Кастомный менеджер модели пользователя,
+	где адрес электронной почты является уникальным идентификатором для аутентификации.
+	"""
+
+	def create_user(self, email, password=None, **extra_fields):
+		""" Создание и сохранение пользователя с указанной почтой и паролем. """
+		if not email:
+			raise ValueError('Поле с электронной почтой должен быть заполнено!')
+		email = self.normalize_email(email)
+		user = self.model(email=email, **extra_fields)
+		user.set_password(password)
+		user.save(using=self._db)
+		# Присваиваем пользователю группу User
+		user_group = Group.objects.get(name="User")
+		user.groups.add(user_group)
+
+		return user
+
+	def create_superuser(self, email, password=None, **extra_fields):
+		""" Создание и сохранение супер пользователя с указанной почтой и паролем. """
+		extra_fields.setdefault('is_staff', True)
+		extra_fields.setdefault('is_superuser', True)
+
+		if extra_fields.get('is_staff') is not True:
+			raise ValueError('Superuser должен иметь флажок is_staff=True.')
+		if extra_fields.get('is_superuser') is not True:
+			raise ValueError('Superuser должен иметь флажок is_superuser=True.')
+
+		return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+	""" Создание пользователя """
+	username = None
+	email = models.EmailField(unique=True, verbose_name="Email", help_text="Введите email")
+	token = models.CharField(max_length=100, verbose_name="Token", blank=True, null=True)
+	is_token_used = models.BooleanField(default=False, verbose_name="Токен использован")
+
+	USERNAME_FIELD = "email"
+	REQUIRED_FIELDS = []
+
+	objects = CustomUserManager()
+
+	class Meta:
+		verbose_name = "Пользователь"
+		verbose_name_plural = "Пользователи"
+
+	def __str__(self):
+		return self.email
