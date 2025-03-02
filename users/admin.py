d7cf9d0f0a16b75b08f20a2157698a37292b30c7
mexic <mexic2284@mail.ru>
2025-03-03 00:32:48 +0400

    Регистрация модели User В админке для отображения соответсвующий полей и фильтрации по полю is_active и поиску пользователя по email.

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """ Регистрация для отображения пользователя в админ панели. """
+    list_display = ("id", "email",)
+    list_filter = ("is_active",)
+    search_fields = ("email",)
