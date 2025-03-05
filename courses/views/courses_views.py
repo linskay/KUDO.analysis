ba95ebdb921a6159c7ad56efe81f580199ff95e9
Serg_Nout <kuchukov.sergey@gmail.com>
2025-03-05 20:41:42 +0300

    fixed Courses

--- courses/views/courses_views.py
+++ courses/views/courses_views.py
@@ -8,19 +8,22 @@ from ..forms import CoursesForm
 import os
 
 class CoursesListView(ListView):
+    """ Выводит список назначенных студенту курсов """
     model = Course
     context_object_name = 'courses'
     paginate_by = 12
     template_name = os.path.join('courses', 'courses_list.html')
     extra_context = {"active_menu": "client", "xxx": "Назначенные курсы"}
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['courses'] = Course.objects.filter(courseandstudents__user_id=self.request.user.pk).exclude(courseandstudents__finish_date=None)
-        return context
+    # фильтрация курсов
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(courseandstudents__user_id=self.request.user.pk, courseandstudents__finish_date=None)
+
 
 
 class CoursesListAllView(ListView):
+    """ Выводит список всех курсов """
     model = Course
     context_object_name = 'courses'
     paginate_by = 12
@@ -29,16 +32,24 @@ class CoursesListAllView(ListView):
 
 
 class CoursesListCompleteView(ListView):
+    """ Выводит список законченных студентом курсов """
     model = Course
     context_object_name = 'courses'
     paginate_by = 12
     template_name = os.path.join('courses', 'courses_list.html')
     extra_context = {"active_menu": "client", "xxx": "выполненые курсы"}
 
-    def get_context_data(self, **kwargs):
+    """def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['courses'] = Course.objects.filter(courseandstudents__user_id=self.request.user.pk, courseandstudents__finish_date=None)
-        return context
+        return context"""
+
+    # фильтрация курсов
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(courseandstudents__user_id=self.request.user.pk).exclude(
+            courseandstudents__finish_date=None)
+
 
 class CoursesDetailView(DetailView):
     model = Course
@@ -48,6 +59,7 @@ class CoursesDetailView(DetailView):
 
 
 class CoursesUpdateView(UpdateView):
+    """ Редактируем курс """
     model = Course
     form_class = CoursesForm
     #fields = ['name_course', 'description_course', 'photo_course', 'pay_course', 'students']
@@ -72,13 +84,11 @@ class CoursesUpdateView(UpdateView):
         if self.photo_course_old:
             if self.photo_course_old != self.object.photo_course:
                 os.remove(self.path_img_temp)
-
-        #if (not self.object.photo_course and self.path_img_temp) or (self.object.photo_course_old != self.object.photo_course):
-           # os.remove(self.path_img_temp)
         return super().form_valid(form)
 
 
 class CoursesCreateView(CreateView):
+    """ Создаем курс """
     model = Course
     form_class = CoursesForm
     #fields = ['name_course','description_course','photo_course','pay_course','students']
@@ -88,6 +98,7 @@ class CoursesCreateView(CreateView):
     # extra_context = {"active_menu": "client"}
 
 class CoursesDeleteView(DeleteView):
+    """ Удаляем курс"""
     model = Course
     context_object_name = 'course'
     success_url = reverse_lazy('courses:courses')
