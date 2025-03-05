3d77d62e5cad9171d5746a0c869535d07efedfdc
Nurlan <nurlan.grachyov@mail.ru>
2025-03-05 15:21:47 +0400

    добавил модель урока и системы прогрессии

--- lessons/models.py
+++ lessons/models.py
@@ -6,15 +6,15 @@ from ..config.settings import AUTH_USER_MODEL
 
 class Lesson(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE, blank=True, null=True)
-    lesson_number = models.IntegerField(verbose_name='номер урока')
+    lesson_id = models.IntegerField(primary_key=True, verbose_name='номер урока')
     title = models.CharField(max_length=100, verbose_name='название урока')
     description = models.TextField(verbose_name='описание урока', max_length=1000)
-    video = models.FileField(upload_to='videos_uploaded', null=True,
+    video = models.FileField(upload_to='videos_uploaded', null=True, blank=True,
                              validators=[
                                  FileExtensionValidator(allowed_extensions=['MOV', 'avi', 'mp4', 'webm', 'mkv'])])
-    create_at = models.DateField(verbose_name='дата создания', auto_now_add=True)
-    update_at = models.DateField(verbose_name='дата обновления', auto_now=True)
-    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateField(verbose_name='дата создания', auto_now_add=True, null=True, blank=True,)
+    updated_at = models.DateField(verbose_name='дата обновления', auto_now=True, null=True, blank=True,)
+    user = models.ManyToManyField(AUTH_USER_MODEL, blank=True, null=True, through="ProgressionSystem", related_name="students")
 
     def __str__(self):
         return f"{self.user} - {self.course} - {self.title}"
@@ -27,7 +27,7 @@ class Lesson(models.Model):
 class ProgressionSystem(models.Model):
     student = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
     lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
-    date_end = models.DateField(verbose_name='дата завершения',)
+    date_end = models.DateField(verbose_name='дата завершения', null=True, blank=True,)
 
     def __str__(self):
         return f"{self.student.email} - {self.lesson.title} - {self.date_end}"
