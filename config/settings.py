cb1703b900b0ac6640a10828b9146de9acb2fc9c
Alex Pavlov <yukiu217@gmail.com>
2025-03-09 01:01:11 +0600

    separation INSTALLES_APPS list to DJANGO_APPS, PROJECT_APPS and THIRD_PARTY_APPS

--- config/settings.py
+++ config/settings.py
@@ -37,18 +37,31 @@ else:
 
 # Application definition
 
-INSTALLED_APPS = [
+# Джанго стандарные приложения.
+DJANGO_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+]
+
+# Наши приложения.
+PROJECT_APPS = [
     "diary",
     "group",
     "lessons",
     "users",
 ]
+
+# Приложения из вне.
+THIRD_PARTY_APPS = [
+
+]
+
+INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
